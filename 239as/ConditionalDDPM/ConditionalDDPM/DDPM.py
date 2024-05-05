e0a14e9cdfcbfd3b272d6faed0712e220a05c561
simplyarsh <arshnoorfly@ucla.edu>
2024-05-04 19:48:44 -0700

    Changes

--- 239as/ConditionalDDPM/ConditionalDDPM/DDPM.py
+++ 239as/ConditionalDDPM/ConditionalDDPM/DDPM.py
@@ -23,18 +23,22 @@ class ConditionalDDPM(nn.Module):
         #   Outputs:
         #       one dictionary containing the variance schedule
         #       $\beta_t$ along with other potentially useful constants.       
-        beta_t = beta_slope*t_s
-        sqrt_beta_t = torch.sqrt(beta_t)
+        beta_t = beta_slope*t_s.to(dtype=torch.float64)
+        sqrt_beta_t = torch.sqrt(beta_t, )
         alpha_t = 1 - beta_t
         oneover_sqrt_alpha = 1/torch.sqrt(alpha_t)
 
-        alpha_t_bars = []
-        for i in range(t_s.shape[0]):
-            t_s_i = t_s[i].item() 
-            nums = 1 - torch.arange(1, t_s_i+1)*beta_slope
-            alpha_t_bars.append(torch.prod(nums))
-        
-        alpha_t_bar = torch.stack(alpha_t_bars)
+        is_scalar = t_s.dim() == 0
+        if is_scalar:
+            nums = 1 - torch.arange(1, t_s+1)*beta_slope
+            alpha_t_bar = torch.prod(nums)
+        else:
+            alpha_t_bars = []
+            for i in range(t_s.shape[0]):
+                t_s_i = t_s[i].item() 
+                nums = 1 - torch.arange(1, t_s_i+1)*beta_slope
+                alpha_t_bars.append(torch.prod(nums))
+            alpha_t_bar = torch.stack(alpha_t_bars)
 
         sqrt_alpha_bar = torch.sqrt(alpha_t_bar)
         sqrt_oneminus_alpha_bar = torch.sqrt(1 - alpha_t_bar)
@@ -86,6 +90,10 @@ class ConditionalDDPM(nn.Module):
 
     def sample(self, conditions, omega):
         T = self.dmconfig.T
+        num_channels = self.dmconfig.num_channels
+        h, w = self.dmconfig.input_dim
+        device = conditions.device
+
         X_t = None
         # ==================================================== #
         # YOUR CODE HERE:
@@ -98,11 +106,26 @@ class ConditionalDDPM(nn.Module):
         #       omega: conditional guidance weight.
         #   Outputs:
         #       generated_images  
+        X_t = torch.randn(conditions.shape[0], num_channels, h, w).to(dtype=torch.float32, device=device)
+        # conditions = F.one_hot(conditions, num_classes=self.dmconfig.num_classes).to(dtype=torch.float32, device=device)
+        cond_mask = torch.full(conditions.size(), -1).to(dtype=torch.float32, device=device)
 
-        pass
+        with torch.no_grad():
+            for t in range(T, 0, -1):
+                if t > 1: z = torch.randn((num_channels, h, w)).to(dtype=torch.float32, device=device)
+                else: z = torch.zeros((num_channels, h, w)).to(dtype=torch.float32, device=device) 
+                t_arr = torch.full((conditions.shape[0], 1), t).view(-1, 1, 1, 1).to(dtype=torch.float32, device=device)
+                undo_noise = (1 + omega) * self.network(X_t, t_arr, conditions) - omega * self.network(X_t, t_arr, cond_mask)
 
-
+                scheduler_t = self.scheduler(torch.full((conditions.shape[0], 1), t))
+                oneover_sqrt_alpha = scheduler_t["oneover_sqrt_alpha"].view(-1, 1, 1, 1).to(dtype=torch.float32, device=device)
+                print(oneover_sqrt_alpha, "HERE")
+                oneminus_alpha_t = 1 - scheduler_t["alpha_t"].view(-1, 1, 1, 1).to(dtype=torch.float32, device=device)
+                sqrt_oneminus_alpha_bar = scheduler_t["sqrt_oneminus_alpha_bar"].view(-1, 1, 1, 1).to(dtype=torch.float32, device=device)
+                sqrt_beta_t = scheduler_t["sqrt_beta_t"].view(-1, 1, 1, 1).to(dtype=torch.float32, device=device)
+                X_t = oneover_sqrt_alpha * (X_t - oneminus_alpha_t / sqrt_oneminus_alpha_bar * undo_noise) + sqrt_beta_t * z
 
         # ==================================================== #
+        
         generated_images = (X_t * 0.3081 + 0.1307).clamp(0,1) # denormalize the output images
         return generated_images
